31ddadfcea6dac80a21989e864b3acbfa064af56
Mateus <mateuscamargo2908@gmail.com>
2024-04-12 09:17:20 -0300

    comments

--- script.js
+++ script.js
@@ -71,42 +71,6 @@ inputPassword.addEventListener('input', (e) => {
             break;
         }
     }
-    /*
-    if (val === "") {
-        strength = 0;
-
-        for (let i = 0; i < block.length; i ++) {
-            block[i].classList.remove('s1');
-            block[i].classList.remove('s2');
-            block[i].classList.remove('s3');
-            block[i].classList.remove('s4');
-        }
-    }
-    if (val !=  "") {
-        strength = 1;
-        block[0].classList.add('s1');
-    }
-    if (containsNumber(val)) {
-        strength = 2;
-        block[0].classList.add('s2');
-        block[1].classList.add('s2');
-    } 
-    if (containsUppercase(val) &&
-        containsNumber(val)) {
-        strength = 3;
-        block[0].classList.add('s3');
-        block[1].classList.add('s3');
-        block[2].classList.add('s3');
-    }   
-    if (containsSpecialChar(val) && 
-        containsUppercase(val) &&
-        containsNumber(val)) {
-        strength = 4;
-        block[0].classList.add('s4');
-        block[1].classList.add('s4');
-        block[2].classList.add('s4');
-        block[3].classList.add('s4');
-    }   */
 });
 
 /*
